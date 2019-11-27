12f1c37c5a51fd0db18d38d3f6d914b5b8185872
sabneet95 <45975033+sabneet95@users.noreply.github.com>
2019-11-26 20:55:45 -0500

    Add files via upload

---
+++ ClassicalMechanics.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// ClassicalMechanics.xaml.h
+// Declaration of the ClassicalMechanics class
+//
+
+#pragma once
+
+#include "ClassicalMechanics.g.h"
+
+namespace Physics
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class ClassicalMechanics sealed
+	{
+	public:
+		ClassicalMechanics();
+	};
+}
